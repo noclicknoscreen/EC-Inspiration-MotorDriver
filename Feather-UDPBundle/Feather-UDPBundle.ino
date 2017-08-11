7fc17f887802708e2a7600e19fde12d821c23fc6
Dudley smith <vj.dudley.smith@gmail.com>
2017-08-11 16:35:05 +0200

    Hack DHCP (doesn't work with arduino ?)

--- Feather-UDPBundle/Feather-UDPBundle.ino
+++ Feather-UDPBundle/Feather-UDPBundle.ino
@@ -17,8 +17,8 @@
 
 #include <NCNS-ArduinoTools.h>
 
-char ssid[] = "InspirationHub";          // your network SSID (name)
-char pass[] = "Inspiration";                    // your network password
+char ssid[] = "linksys-MedenAgan";          // your network SSID (name)
+char pass[] = "Edwood72";                    // your network password
 
 // A UDP instance to let us send and receive packets over UDP
 WiFiUDP Udp;
@@ -32,27 +32,27 @@ unsigned int receivedPosition = 0;              // LOW means led is *on*
 
 void setup() {
   Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+
   pinMode(ERROR_LED, OUTPUT);
   pinMode(POSTN_LED, OUTPUT);
-  
+
   byte mac[6];
   WiFi.macAddress(mac);
-  Serial.print("MAC Address: ");
-  Serial.print(mac[0], HEX);
-  Serial.print(":");
-  Serial.print(mac[1], HEX);
-  Serial.print(":");
-  Serial.print(mac[2], HEX);
-  Serial.print(":");
-  Serial.print(mac[3], HEX);
-  Serial.print(":");
-  Serial.print(mac[4], HEX);
-  Serial.print(":");
-  Serial.println(mac[5], HEX);
+  IPAddress ip = getIp(mac);  // Node static IP
+  // Update these with values suitable for your network.
+  IPAddress gateway(192, 168, 2, 1);
+  IPAddress subnet(255, 255, 255, 0);
 
   // Connect to WiFi network
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+  Serial.print("Connecting to SSID [");
+  Serial.print(ssid);
+  Serial.print("] pass [");
+  Serial.print(pass);
+  Serial.println("]");
+
+  WiFi.config(ip, gateway, subnet);
   WiFi.begin(ssid, pass);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -74,10 +74,53 @@ void setup() {
 
 }
 
+IPAddress getIp(byte _mac[6]) {
+
+  IPAddress _ip;
+
+  String strMac;
+  strMac += String(_mac[0], HEX);
+  strMac += ":";
+  strMac += String(_mac[1], HEX);
+  strMac += ":";
+  strMac += String(_mac[2], HEX);
+  strMac += ":";
+  strMac += String(_mac[3], HEX);
+  strMac += ":";
+  strMac += String(_mac[4], HEX);
+  strMac += ":";
+  strMac += String(_mac[5], HEX);
+  strMac.toUpperCase();
+  
+  Serial.print("MAC Address: ");
+  Serial.println(strMac);
+
+  // My Own DHCP Table :((((
+  if (strMac == "60:1:94:19:ec:a8") {
+    // Feather 1
+    _ip = IPAddress(192, 168, 2, 12);
+  } else if (strMac == "5C:CF:7F:3A:1B:8E") {
+    // Feather 2
+    _ip = IPAddress(192, 168, 2, 13);
+  } else if (strMac == "5C:CF:7F:3A:39:41") {
+    // Feather 3
+    _ip = IPAddress(192, 168, 2, 14);
+  } else if (strMac == "5C:CF:7F:3A:2D:73") {
+    // Feather 4
+    _ip = IPAddress(192, 168, 2, 15);
+  }
+
+  Serial.print("Obtained IP is : ");
+  Serial.println(_ip);
+
+  return _ip;
+
+}
+
 void positionChange(OSCMessage &msg) {
   // Possibly issue onto Millumin, so constrain the values
   float msgContent = constrain(msg.getFloat(0), 0.0, 1.0);
-  
+
   receivedPosition = 255 * msgContent;
   analogWrite(POSTN_LED, receivedPosition);
 
